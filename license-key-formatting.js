38dfc49d330a015cc03d1221c163068121078a0a
Jady Rufino <jadyrufa@gmail.com>
2023-12-01 12:40:21 -0300

    chore: rule improved

--- license-key-formatting.js
+++ license-key-formatting.js
@@ -5,8 +5,17 @@ function formatLicenseKey(S, K) {
 
   const cleanString = S.replace(/-/g, '').toUpperCase()
 
+  const firstGroupSize = cleanString.length % K || K
+
   const groups = []
-  for (let i = 0; i < cleanString.length; i += K) {
+  let index = 0
+
+  if (firstGroupSize !== K) {
+    groups.push(cleanString.slice(0, firstGroupSize))
+    index = firstGroupSize
+  }
+
+  for (let i = index; i < cleanString.length; i += K) {
     groups.push(cleanString.slice(i, i + K))
   }
 
